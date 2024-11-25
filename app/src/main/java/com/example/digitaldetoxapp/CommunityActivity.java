87063643e3ebce83aa354118a434107a140d4558
dudck <dudcks0529@gmail.com>
2024-11-25 15:45:44 +0900

    add Community

--- app/src/main/java/com/example/digitaldetoxapp/CommunityActivity.java
+++ app/src/main/java/com/example/digitaldetoxapp/CommunityActivity.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class CommunityActivity extends AppCompatActivity {
 
     private Button homeButton;
+    private CardView userPostCard, rankingCard, top10Card, reviewCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,10 @@ public class CommunityActivity extends AppCompatActivity {
         setContentView(R.layout.activity_community); // 커뮤니티 레이아웃 설정
 
         homeButton = findViewById(R.id.button_home);
+        userPostCard = findViewById(R.id.card_user_post);
+        rankingCard = findViewById(R.id.card_ranking);
+        top10Card = findViewById(R.id.card_top10);
+        reviewCard = findViewById(R.id.card_review);
 
         // 홈 버튼 클릭 리스너 설정
         homeButton.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +32,42 @@ public class CommunityActivity extends AppCompatActivity {
                 Intent intent = new Intent(CommunityActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        // User Post Card 클릭 리스너 설정
+        userPostCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CommunityActivity.this, UserPostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Ranking Card 클릭 리스너 설정
+        rankingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CommunityActivity.this, RankingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Top 10 Card 클릭 리스너 설정
+        top10Card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CommunityActivity.this, TopTenActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Review Card 클릭 리스너 설정
+        reviewCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CommunityActivity.this, ReviewActivity.class);
+                startActivity(intent);
             }
         });
     }
